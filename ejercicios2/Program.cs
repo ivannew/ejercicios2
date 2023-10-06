23b8cafe0dc16bb8685c16214674f87998a3e719
FranciZco GuZmán <FranciZco GuZmn@DESKTOP-HH5SITF>
2023-10-05 21:26:17 -0700

    Agregar archivos de proyecto.

---
+++ ejercicios2/Program.cs
@@ -0,0 +1,131 @@
+﻿using ejercicios2.metodos;
+
+namespace ejercicios2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //EJERCICIO 1
+            /* ejercicios ejercicios1 = new ejercicios();
+             int[] resultado = ejercicios1.SumaDeArreglos();
+             Console.WriteLine("Resultado de la suma:");
+             ejercicios1.ImprimirArreglo(resultado);*/
+
+            // EJERICIO 2
+            /* ejercicios ejercicios1 = new ejercicios();
+             Console.WriteLine("Ingrese el tamaño del arreglo:");
+             int tamañoArreglo = int.Parse(Console.ReadLine());
+
+             int[] arreglo = new int[tamañoArreglo];
+
+             for (int i = 0; i < tamañoArreglo; i++)
+             {
+                 Console.Write($"Ingrese el valor {i + 1}: ");
+                 arreglo[i] = int.Parse(Console.ReadLine());
+             }
+
+             Console.WriteLine("Ingrese el elemento a buscar:");
+             int elementoBuscado = int.Parse(Console.ReadLine());
+
+             int[] posicionesCoincidencia = ejercicios1.BuscarElemento(arreglo, elementoBuscado);
+
+             Console.WriteLine("Posiciones de coincidencia:");
+             foreach (int posicion in posicionesCoincidencia)
+             {
+                 Console.WriteLine($"Posición: {posicion}");
+             } */
+            //EJERCICIO 4
+
+            /* ejercicios ejercicios1 = new ejercicios();
+
+             Console.WriteLine("Ingrese el tamaño del arreglo:");
+             int tamañoArreglo = int.Parse(Console.ReadLine());
+
+             if (tamañoArreglo <= 0)
+             {
+                 Console.WriteLine("El tamaño del arreglo debe ser mayor que cero.");
+                 return;
+             }
+
+             int[] arreglo = ejercicios1.CrearArreglo(tamañoArreglo);
+
+             Console.WriteLine("\nContenido del arreglo:");
+
+             foreach (int valor in arreglo)
+             {
+                 Console.Write(valor + " ");
+             }
+
+             Console.WriteLine();
+         }
+            */
+            //EJERCICIO 5
+            /* ejercicios ejercicios1 = new ejercicios();
+             Console.Write("Ingrese la cantidad de notas: ");
+             int N = int.Parse(Console.ReadLine());
+
+             if (N <= 0)
+             {
+                 Console.WriteLine("La cantidad de notas debe ser mayor que cero.");
+                 return;
+             }
+
+             int[] notas = new int[N];
+
+             // Llenar el arreglo con las notas ingresadas por el usuario.
+             for (int i = 0; i < N; i++)
+             {
+                 Console.Write($"Ingrese la nota {i + 1}: ");
+                 notas[i] = int.Parse(Console.ReadLine());
+             }
+
+             int aprobadas, desaprobadas;
+             double promedioAprobadas, promedioDesaprobadas;
+
+             ejercicios1.CalcularEstadisticasNotas(notas, out aprobadas, out desaprobadas, out promedioAprobadas, out promedioDesaprobadas);
+
+             ejercicios1.MostrarResumenNotas(aprobadas, desaprobadas, promedioAprobadas, promedioDesaprobadas);
+            */
+
+            //EJERCICIOS 3 
+            /*ejercicios ejercicios1 = new ejercicios();
+            Console.Write("Ingrese la cantidad de alumnos para Álgebra: ");
+            int cantidadAlumnosAlgebra = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la cantidad de alumnos para Análisis: ");
+            int cantidadAlumnosAnalisis = int.Parse(Console.ReadLine());
+
+            int[] algebra = new int[cantidadAlumnosAlgebra];
+            int[] analisis = new int[cantidadAlumnosAnalisis];
+
+            // Ingresar IDs de alumnos para Álgebra.
+            IngresarAlumnos(algebra, "Álgebra");
+
+            // Ingresar IDs de alumnos para Análisis.
+            IngresarAlumnos(analisis, "Análisis");
+
+            // Encontrar y mostrar a los alumnos inscritos en ambas asignaturas.
+            int[] alumnosEnAmbasAsignaturas = ejercicios1.EncontrarAlumnosEnAmbasAsignaturas(algebra, analisis);
+
+            Console.WriteLine("\nAlumnos inscritos en ambas asignaturas:");
+            foreach (int alumno in alumnosEnAmbasAsignaturas)
+            {
+                Console.WriteLine($"ID: {alumno}");
+            }
+
+            Console.WriteLine($"Número de alumnos que se repiten: {alumnosEnAmbasAsignaturas.Length}");
+        }
+
+        static void IngresarAlumnos(int[] arreglo, string asignatura)
+        {
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                Console.Write($"Ingrese el ID del alumno {i + 1} para {asignatura}: ");
+                arreglo[i] = int.Parse(Console.ReadLine());
+            }
+        }*/
+        }
+
+    }
+}
---
+++ ejercicios2/metodos/ejercicios.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicios2.metodos
+{
+    internal class ejercicios
+    {
+        public int[] SumaDeArreglos()
+        {
+            Console.WriteLine("Escribe el tamaño del arreglo");
+            int tamañoArreglos = Convert.ToInt32(Console.ReadLine());
+
+            int[] arreglo1 = new int[tamañoArreglos];
+            int[] arreglo2 = new int[tamañoArreglos];
+            int[] resultado = new int[tamañoArreglos];
+
+            Console.WriteLine("Número para el primer arreglo");
+            for (int i = 0; i < tamañoArreglos; i++)
+            {
+                Console.WriteLine($"Valor {i + 1}:");
+                arreglo1[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Ahora escribe el segundo arreglo");
+            for (int i = 0; i < tamañoArreglos; i++)
+            {
+                Console.WriteLine($"Valor {i + 1}:");
+                arreglo2[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < tamañoArreglos; i++)
+            {
+                resultado[i] = arreglo1[i] + arreglo2[i];
+            }
+
+       
+            return resultado;
+        }
+        public void ImprimirArreglo(int[] arreglo)
+        {
+            foreach (int valor in arreglo)
+            {
+                Console.Write(valor + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public int[] BuscarElemento(int[] arreglo, int elementoBuscado)
+        {
+            int contadorCoincidencia = 0;
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                if (arreglo[i] == elementoBuscado)
+                {
+                    contadorCoincidencia++;
+                }
+            }
+
+            int[] posicionesCoincidencia = new int[contadorCoincidencia];
+            contadorCoincidencia = 0;
+
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                if (arreglo[i] == elementoBuscado)
+                {
+                    posicionesCoincidencia[contadorCoincidencia] = i;
+                    contadorCoincidencia++;
+                }
+            }
+
+            return posicionesCoincidencia;
+        }
+        private List<int> alumnos = new List<int>();
+
+        public void IngresarAlumnos()
+        {
+            Console.WriteLine("Ingrese los IDs de los alumnos para Álgebra (separados por espacios):");
+            string[] input = Console.ReadLine().Split();
+
+            foreach (string id in input)
+            {
+                int alumnoID = int.Parse(id);
+                if (!alumnos.Contains(alumnoID))
+                {
+                    alumnos.Add(alumnoID);
+                }
+                else
+                {
+                    Console.WriteLine($"El alumno con ID {alumnoID} ya está inscrito en Álgebra.");
+                }
+            }
+        }
+
+        public IEnumerable<int> ObtenerAlumnos()
+        {
+            return alumnos;
+        }
+        // NO ENTENDIDO POR COMPLETO
+        public  int[] CrearArreglo(int tamaño)
+        {
+            int[] arreglo = new int[tamaño];
+            for (int i = 0; i < tamaño; i++)
+            {
+                int valor;
+                do
+                {
+                    Console.Write($"Ingrese el valor {i + 1}: ");
+                    valor = int.Parse(Console.ReadLine());
+
+                    if (Array.Exists(arreglo, element => element == valor))
+                    {
+                        Console.WriteLine("El valor ya existe en el arreglo. Intente nuevamente.");
+                    }
+                } while (Array.Exists(arreglo, element => element == valor));
+
+                arreglo[i] = valor;
+            }
+            return arreglo;
+        }
+        // sacado con ayuda
+        public void CalcularEstadisticasNotas(int[] notas, out int aprobadas, out int desaprobadas, out double promedioAprobadas, out double promedioDesaprobadas)
+        {
+            aprobadas = 0;
+            desaprobadas = 0;
+            int sumaAprobadas = 0;
+            int sumaDesaprobadas = 0;
+
+            foreach (int nota in notas)
+            {
+                if (nota >= 0 && nota <= 5)
+                {
+                    desaprobadas++;
+                    sumaDesaprobadas += nota;
+                }
+                else if (nota >= 6 && nota <= 15)
+                {
+                    aprobadas++;
+                    sumaAprobadas += nota;
+                }
+                else if (nota >= 16 && nota <= 20)
+                {
+                    aprobadas++;
+                    sumaAprobadas += nota;
+                }
+            }
+
+            promedioAprobadas = aprobadas > 0 ? (double)sumaAprobadas / aprobadas : 0;
+            promedioDesaprobadas = desaprobadas > 0 ? (double)sumaDesaprobadas / desaprobadas : 0;
+        }
+
+        public void MostrarResumenNotas(int aprobadas, int desaprobadas, double promedioAprobadas, double promedioDesaprobadas)
+        {
+            Console.WriteLine("\nResumen de notas:");
+            Console.WriteLine($"Cantidad de notas aprobadas: {aprobadas}");
+            Console.WriteLine($"Cantidad de notas desaprobadas: {desaprobadas}");
+            Console.WriteLine($"Promedio de notas aprobadas: {promedioAprobadas:F2}");
+            Console.WriteLine($"Promedio de notas desaprobadas: {promedioDesaprobadas:F2}");
+        }
+       public int[] EncontrarAlumnosEnAmbasAsignaturas(int[] arreglo1, int[] arreglo2)
+        {
+            int[] alumnosEnAmbasAsignaturas = new int[Math.Min(arreglo1.Length, arreglo2.Length)];
+            int contador = 0;
+
+            foreach (int alumno1 in arreglo1)
+            {
+                foreach (int alumno2 in arreglo2)
+                {
+                    if (alumno1 == alumno2)
+                    {
+                        alumnosEnAmbasAsignaturas[contador] = alumno1;
+                        contador++;
+                        break; // Para evitar agregar el mismo alumno varias veces.
+                    }
+                }
+            }
+
+            Array.Resize(ref alumnosEnAmbasAsignaturas, contador);
+
+            return alumnosEnAmbasAsignaturas;
+        }
+
+        public void IngresarAlumnos(int[] arreglo, string asignatura)
+        {
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                Console.Write($"Ingrese el ID del alumno {i + 1} para {asignatura}: ");
+                arreglo[i] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
